04da40ca75335e9d392f439e3880623af9d91a87
Adam Edmunds <mightylordx786@gmail.com>
2021-01-11 10:38:00 +0000

    Revert "Updated app/functions scripts"
    This reverts commit 251eeb8faf173c267bc232a918a088133566a1eb.

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
-from functions import *
+from functions import foo
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    if is_logged_in(0):
-        return render_template("index.html")
-    else:
-        return render_template("login.html")
-
+    foo()
+    return render_template("index.html")
 
 @app.route('/contact')
 def contact_page():
